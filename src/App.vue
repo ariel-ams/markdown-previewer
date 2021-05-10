78949bda2dc2b10c27641b9e73bcc2dde9067742
Ariel Medina <arielm92@gmail.com>
2021-05-10 16:51:34 -0300

    Proyecto inicial - markdown previewer

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="main">
+    <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
+
+    <div id="preview">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#main {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
