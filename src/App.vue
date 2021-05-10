87757e548a7d0832d1eeae9002e0bd832e442d35
Ariel Medina <arielm92@gmail.com>
2021-05-10 19:30:50 -0300

    Agregado componente MarkdownPreview para visualizar texto de markdown con marked

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="main">
-    <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
+    <textarea name="editor" id="editor" cols="30" rows="10" v-model="markdown"></textarea>
 
-    <div id="preview">
-      
-    </div>
+    <MarkdownPreview :markdown="markdown"></MarkdownPreview>
   </div>
 </template>
 
 <script>
+import MarkdownPreview from "./components/markdown-preview";
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
+    MarkdownPreview
+  },
+  data(){
+    return {
+      markdown: ''
+    }
+  },
+  mounted(){
+    axios.get('https://gist.githubusercontent.com/cuonggt/9b7d08a597b167299f0d/raw/c872b3fb5f851d00fba18dfbd456179319a3e9fe/markdown_guide.md')
+      .then(response => {
+        this.markdown = response.data;
+      })
   }
 }
 </script>
@@ -24,6 +36,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#main {
+  display: grid;
+  grid-template-columns: 50% 50%;
+  max-height: 100%;
+  overflow: scroll;
+}
+
+#main textarea{
+  max-height: 100%;
+  overflow: scroll;
 }
 </style>
---
+++ src/components/markdown-preview.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-html="parsedHtml"></div>
+</template>
+
+<script>
+import marked from "marked";
+
+export default {
+    props:{
+        markdown:{
+            type: String,
+            require:true,
+            default: ''
+        }
+    },
+    computed:{
+        parsedHtml(){
+            return marked(this.markdown);
+        }
+  }
+}
+</script>
+
+<style>
+
+</style>
